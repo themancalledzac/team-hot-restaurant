6520958f8b7e48fdb3c95db13cb2bb9ebf8cb879
Zac Edens <themancalledzac@gmail.com>
2020-12-14 19:10:50 -0800

    added table.html and const tables

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 3001;
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// API Table List
+const tables = [];
+
 
 // ROUTES
 
@@ -26,6 +30,8 @@ app.get("/reserve", function(req, res) {
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
+
+
 
 
 // Listener
